97eb8c2c1ee3e0fe029822ae04d3c1ece0484249
NateRive <naterive1125@gmail.com>
2020-06-07 17:53:38 +0900

    changed

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func NewRouter() *echo.Echo {
 }
 
 func Handler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello1111")
+	return c.String(http.StatusOK, "Hello2222")
 }
 
 func main() {
