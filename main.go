a7cb1ffb2a641dd7c3201b4436fce9861688cf2e
taros <yu04u2ki0win@gmail.com>
2020-06-07 11:10:06 +0900

    change iwase

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func NewRouter() *echo.Echo {
 }
 
 func Handler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello9999")
+	return c.String(http.StatusOK, "Hello0000")
 }
 
 func main() {
